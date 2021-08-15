0e4d56fb645e9327ec5571447a4d89833318062f
Michael Hsieh <opas1216@hotmail.com>
2021-08-15 22:27:08 +0800

    Daily workload upload Update - Chapter 1

--- main.py
+++ main.py
@@ -1,16 +1,85 @@
-# This is a sample Python script.
+import sys
+import scipy
+import numpy as np
+import matplotlib
+import pandas
+from sklearn import preprocessing, model_selection
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, accuracy_score
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+import pandas as pd
+import ssl
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+ssl._create_default_https_context = ssl._create_unverified_context
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data"
+names = ['id', 'clump_thickness', 'uniform_cell_size', 'uniform_cell_shape', 'marginal_adhesion',
+         'single_epithelial_size', 'bare_nuclei', 'bland_chromatin', 'normal_nucleoli', 'mitoses', 'class']
+
+# df is Data Frame
+df = pd.read_csv(url, names=names)
+df.replace('?', -99999, inplace=True)
+print(df.axes)
+
+df.drop(['id'], 1, inplace=True)
+print(df.shape)
+print(df.loc[6])
+print("====================")
+print(df.describe())
+# figsize for changing the size of the figure, and hist means histogram.
+df.hist(figsize=(10, 10))
+# plt.show()
+
+scatter_matrix(df, figsize=(18, 18))
+# plt.show()
+
+# Create X and Y dataset for training
+X = np.array(df.drop(['class'], 1))
+y = np.array(df['class'])
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+# Testing Options
+seed = 8
+scoring = 'accuracy'
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+models = []
+models.append(('KNN', KNeighborsClassifier(n_neighbors=5)))
+models.append(('SVM', SVC()))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# evaluate each model in turn
+results = []
+names = []
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for name, model in models:
+    kfold = model_selection.KFold(n_splits=10, random_state=seed, shuffle=True)
+    cv_results = model_selection.cross_val_score(model, X_train, y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" %(name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+
+# Make predictions on validation dataset
+
+for name, model in models:
+    model.fit(X_train, y_train)
+    predictions = model.predict(X_test)
+    print(name)
+    print(accuracy_score(y_test, predictions))
+    print(classification_report(y_test, predictions))
+
+
+clf = SVC()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
+
+example_measures = np.array([[4, 2, 1, 1, 1, 2, 3, 2, 1]])
+example_measures = example_measures.reshape(len(example_measures), -1)
+prediction = clf.predict(example_measures)
+print("Shape = {}".format(example_measures.shape))
+print(prediction)
